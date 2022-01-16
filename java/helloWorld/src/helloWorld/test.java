fe18f9114dcbf83d1d71fe8dc7342f2d84fca49c
unpi <eun930601@gmail.com>
2022-01-16 17:29:23 +0900

    java 프로젝트 연습용

---
+++ java/helloWorld/src/helloWorld/test.java
@@ -0,0 +1,36 @@
+package helloWorld;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		// test1330();
+		// test10718();
+
+	}
+	
+	private static void test1330() {
+		int a, b;
+
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		if (a > b)
+			System.out.print(">");
+		else if (a < b)
+			System.out.print("<");
+		else
+			System.out.print("==");
+	}
+	
+	private static void test10718() {
+		
+		System.out.println("강한친구 대한육군\n".repeat(2));
+		
+	}
+	
+
+}
